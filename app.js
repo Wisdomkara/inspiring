4385b0569d19e58f8cf41e40e7c2c4b907c33149
Wisdom Kara <wiskara1@gmail.com>
2024-09-15 23:25:56 +0100

    hgdh

--- app.js
+++ app.js
@@ -1077,19 +1077,16 @@ const breakpoint = 768;
 toggleBtn.addEventListener("click", function () {
   dropdown.classList.toggle("active");
 });
-// toggleBtn.addEventListener('click',function () {
-//   if (dropdown.classList.contains('active')) {
-//     dropdown.classList.remove('active')
 
-//   }else{dropdown.classList.add('active')}
-// })
 function handleResize() {
   if (window.innerWidth >= breakpoint) {
     dropdown.classList.remove("active");
   }
+  
 }
 window.addEventListener("resize", handleResize);
 window.addEventListener("DOMContentLoaded", handleResize);
+
 
 // for relationship container
 
